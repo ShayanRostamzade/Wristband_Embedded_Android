262588cba1821f30b66536a28a6252e47cf71f48
Shayan <shayanrostamzade@Shayans-MacBook-Pro.local>
2021-07-17 15:33:53 +0430

    first commit

---
+++ 3rd_Party_Drivers/inc/ssd1306.h
@@ -0,0 +1,187 @@
+#ifndef SSD1306_H
+#define SSD1306_H 100
+
+/* C++ detection */
+#ifdef __cplusplus
+extern C {
+#endif
+
+/**
+ * This SSD1306 LCD uses I2C for communication
+ *
+ * Library features functions for drawing lines, rectangles and circles.
+ *
+ * It also allows you to draw texts and characters using appropriate functions provided in library.
+ *
+ * Default pinout
+ *
+SSD1306    |STM32F10x    |DESCRIPTION
+
+VCC        |3.3V         |
+GND        |GND          |
+SCL        |PB6          |Serial clock line
+SDA        |PB7          |Serial data line
+ */
+
+#include "stm32f1xx_hal.h"
+
+#include "fonts.h"
+
+#include "stdlib.h"
+#include "string.h"
+
+
+/* I2C address */
+#ifndef SSD1306_I2C_ADDR
+#define SSD1306_I2C_ADDR         0x78
+//#define SSD1306_I2C_ADDR       0x7A
+#endif
+
+/* SSD1306 settings */
+/* SSD1306 width in pixels */
+#ifndef SSD1306_WIDTH
+#define SSD1306_WIDTH            128
+#endif
+/* SSD1306 LCD height in pixels */
+#ifndef SSD1306_HEIGHT
+#define SSD1306_HEIGHT           64
+#endif
+
+/**
+ * @brief  SSD1306 color enumeration
+ */
+typedef enum {
+	SSD1306_COLOR_BLACK = 0x00, /*!< Black color, no pixel */
+	SSD1306_COLOR_WHITE = 0x01  /*!< Pixel is set. Color depends on LCD */
+} SSD1306_COLOR_t;
+
+
+
+/**
+ * @brief  Initializes SSD1306 LCD
+ * @param  None
+ * @retval Initialization status:
+ *           - 0: LCD was not detected on I2C port
+ *           - > 0: LCD initialized OK and ready to use
+ */
+uint8_t SSD1306_Init(void);
+
+/** 
+ * @brief  Updates buffer from internal RAM to LCD
+ * @note   This function must be called each time you do some changes to LCD, to update buffer from RAM to LCD
+ * @param  None
+ * @retval None
+ */
+void SSD1306_UpdateScreen(void);
+
+/**
+ * @brief  Toggles pixels invertion inside internal RAM
+ * @note   @ref SSD1306_UpdateScreen() must be called after that in order to see updated LCD screen
+ * @param  None
+ * @retval None
+ */
+void SSD1306_ToggleInvert(void);
+
+/** 
+ * @brief  Fills entire LCD with desired color
+ * @note   @ref SSD1306_UpdateScreen() must be called after that in order to see updated LCD screen
+ * @param  Color: Color to be used for screen fill. This parameter can be a value of @ref SSD1306_COLOR_t enumeration
+ * @retval None
+ */
+void SSD1306_Fill(SSD1306_COLOR_t Color);
+
+/**
+ * @brief  Draws pixel at desired location
+ * @note   @ref SSD1306_UpdateScreen() must called after that in order to see updated LCD screen
+ * @param  x: X location. This parameter can be a value between 0 and SSD1306_WIDTH - 1
+ * @param  y: Y location. This parameter can be a value between 0 and SSD1306_HEIGHT - 1
+ * @param  color: Color to be used for screen fill. This parameter can be a value of @ref SSD1306_COLOR_t enumeration
+ * @retval None
+ */
+void SSD1306_DrawPixel(uint16_t x, uint16_t y, SSD1306_COLOR_t color);
+
+/**
+ * @brief  Sets cursor pointer to desired location for strings
+ * @param  x: X location. This parameter can be a value between 0 and SSD1306_WIDTH - 1
+ * @param  y: Y location. This parameter can be a value between 0 and SSD1306_HEIGHT - 1
+ * @retval None
+ */
+void SSD1306_SetPosition(uint16_t x, uint16_t y);
+
+/**
+ * @brief  Puts character to internal RAM
+ * @note   @ref SSD1306_UpdateScreen() must be called after that in order to see updated LCD screen
+ * @param  ch: Character to be written
+ * @param  *Font: Pointer to @ref FontDef_t structure with used font
+ * @param  color: Color used for drawing. This parameter can be a value of @ref SSD1306_COLOR_t enumeration
+ * @retval Character written
+ */
+char SSD1306_PrintCharacter(char ch, FontDef_t* Font, SSD1306_COLOR_t color);
+
+/**
+ * @brief  Puts string to internal RAM
+ * @note   @ref SSD1306_UpdateScreen() must be called after that in order to see updated LCD screen
+ * @param  *str: String to be written
+ * @param  *Font: Pointer to @ref FontDef_t structure with used font
+ * @param  color: Color used for drawing. This parameter can be a value of @ref SSD1306_COLOR_t enumeration
+ * @retval Zero on success or character value when function failed
+ */
+char SSD1306_PrintString(char* str, FontDef_t* Font, SSD1306_COLOR_t color);
+
+
+#ifndef ssd1306_I2C_TIMEOUT
+#define ssd1306_I2C_TIMEOUT					20000
+#endif
+
+/**
+ * @brief  Initializes SSD1306 LCD
+ * @param  None
+ * @retval Initialization status:
+ *           - 0: LCD was not detected on I2C port
+ *           - > 0: LCD initialized OK and ready to use
+ */
+void ssd1306_I2C_Init();
+
+/**
+ * @brief  Writes single byte to slave
+ * @param  *I2Cx: I2C used
+ * @param  address: 7 bit slave address, left aligned, bits 7:1 are used, LSB bit is not used
+ * @param  reg: register to write to
+ * @param  data: data to be written
+ * @retval None
+ */
+void ssd1306_I2C_Write(uint8_t address, uint8_t reg, uint8_t data);
+
+/**
+ * @brief  Writes multi bytes to slave
+ * @param  *I2Cx: I2C used
+ * @param  address: 7 bit slave address, left aligned, bits 7:1 are used, LSB bit is not used
+ * @param  reg: register to write to
+ * @param  *data: pointer to data array to write it to slave
+ * @param  count: how many bytes will be written
+ * @retval None
+ */
+void ssd1306_I2C_WriteMulti(uint8_t address, uint8_t reg, uint8_t *data, uint16_t count);
+
+
+void SSD1306_ScrollRight(uint8_t start_row, uint8_t end_row);
+
+
+void SSD1306_StopScroll(void);
+
+// inverts the display i = 1->inverted, i = 0->normal
+
+void SSD1306_InvertDisplay (int i);
+
+
+// clear the display
+
+void SSD1306_Clear (void);
+
+
+/* C++ detection */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
